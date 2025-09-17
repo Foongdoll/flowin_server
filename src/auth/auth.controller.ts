6cf878fd36c3a5f1b943d17ce28387fa66a2c637
신현우 <healim5028@naver.com>
2025-09-17 16:03:33 +0900

    feat: 요청 응답 DTO 클래스 벨리데이션 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,33 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt.guard';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 class RegisterDto {
+  @IsEmail()
   email!: string;
+  @IsString()
   name!: string;
+
+  @IsString()
+  @MinLength(6)
   password!: string;
 }
 
 class LoginDto {
+  @IsEmail()
   email!: string;
+  @IsString()
+  @MinLength(6)
   password!: string;
 }
 
 @Controller('auth')
 export class AuthController {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) { }
 
   @Post('register')
-  register(@Body() dto: RegisterDto) {
+  register(@Body() dto: RegisterDto) {    
     return this.auth.register(dto.email, dto.name, dto.password);
   }
 
